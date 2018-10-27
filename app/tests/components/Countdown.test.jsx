ce13fcf87a5c31da6ceab712e016c87385a702f3
Rahul Kumar <rahul@Rahuls-MacBook-Pro.local>
2018-10-28 01:44:42 +0530

    df

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -32,7 +32,7 @@ describe('Countdown', () => {
             setTimeout(() => {
                 expect(countdown.state.count).toBe(0);
                 done();
-            }, 3001)
+            }, 1001)
         });
     });
 });
