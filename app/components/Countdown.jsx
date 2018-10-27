801797ace51312c75dde03c9e607624f0d23d874
Rahul Kumar <rahul@Rahuls-MacBook-Pro.local>
2018-10-27 15:11:43 +0530

    add nav router

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var Countdown = React.createClass({
+    render: function () {
+        return (
+            <p>Countdown.jsx</p>
+        ) 
+    }
+});
+
+module.exports = Countdown;
+
