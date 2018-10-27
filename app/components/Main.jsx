0cf1a1ff569f5933e29be63950679c37a8e4ab2e
Rahul Kumar <rahul@Rahuls-MacBook-Pro.local>
2018-10-27 16:46:50 +0530

    countdown form

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -5,10 +5,9 @@ var Main = React.createClass({
     render: function() {
         return(
             <div>
-                <div>
-                    <div>
-                        <Navigation/>
-                        <p>Hello</p>
+                 <Navigation/>
+                <div className = "row">
+                    <div className = "column small-centered medium-6 large-4">
                         {this.props.children}
                     </div>
                 </div>
