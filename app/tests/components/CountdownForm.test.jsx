01ffcea1b6f656a04605740e43156f86d7ec9fb2
Rahul Kumar <rahul@Rahuls-MacBook-Pro.local>
2018-10-27 22:38:40 +0530

    add countdown cm

--- app/tests/components/CountdownForm.test.jsx
+++ app/tests/components/CountdownForm.test.jsx
@@ -10,5 +10,26 @@ describe('CountdownForm', () => {
     it('should exist', () => {
         expect(CountdownForm).toExist();
     });
-    
+
+    it('should call onSetCountdown if valid seconds enterd', () => {
+        var spy = expect.createSpy();
+        var countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy}/>);
+        var $el = $(ReactDOM.findDOMNode(countdownForm));
+
+        countdownForm.refs.seconds.value = '109';
+        TestUtils.Simulate.submit($el.find('form')[0]);
+
+        expect(spy).toHaveBeenCalledWith(109);
+    });
+
+    it('should not call onSetCountdown if invalid seconds enterd', () => {
+        var spy = expect.createSpy();
+        var countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy}/>);
+        var $el = $(ReactDOM.findDOMNode(countdownForm));
+
+        countdownForm.refs.seconds.value = 'aa';
+        TestUtils.Simulate.submit($el.find('form')[0]);
+
+        expect(spy).toNotHaveBeenCalled();
+    });
 });
